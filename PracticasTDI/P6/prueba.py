97801e406623f33923de06e6d390b332fbb749da
anacuevasbr <32259160+anacuevasbr@users.noreply.github.com>
2020-04-27 13:50:11 +0200

    Avances en transformacion HSI

--- PracticasTDI/P6/prueba.py
+++ PracticasTDI/P6/prueba.py
@@ -13,6 +13,7 @@ def RGB2HSI(Img):
     
     eps = 0.00000000001
     Ist = Img/255
+    Ist = np.float32(Ist)
     print('Ist type')
     print(Ist.dtype)
     I_R,I_G,I_B = cv2.split(Ist)
@@ -22,11 +23,16 @@ def RGB2HSI(Img):
     R = I_R / 255
     G = I_G / 255
     B = I_B / 255
-    print(I_R.dtype)
+    print(I_R)
+    print(I_R[0][0])
+    np.savetxt('dataR.csv', I_R, delimiter=',')
+    np.savetxt('dataG.csv', I_G, delimiter=',')
+    np.savetxt('dataB.csv', I_B, delimiter=',')
     h, w = R.shape
 
     #H
     dividendo = ((R-G)+(R-B))/2
+
     
     divisor = np.sqrt((R-G)*(R-G) + (R-B)*(G-B)) + eps
     
@@ -46,7 +52,6 @@ def RGB2HSI(Img):
     #S
     S = 1- (3*np.minimum(R, G, B))/(R+G+B+ eps)
     
-    print('s : ' + str(S.dtype))
     
     I_HSI = cv2.merge((H,S,I))
     
@@ -59,3 +64,4 @@ if __name__ == "__main__":
     I = cv2.cvtColor(I, cv2.COLOR_BGR2RGB)
     
     I_func = RGB2HSI(I)
+    np.savetxt('dataS.csv', I_func, delimiter=',')
