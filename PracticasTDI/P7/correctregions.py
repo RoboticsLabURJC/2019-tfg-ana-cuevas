826a417063e9b00ad7ece2dd940faac21d8407a4
anacuevasbr <32259160+anacuevasbr@users.noreply.github.com>
2021-04-15 10:56:24 +0200

    más intentos de reg max, solucionar bordes

--- PracticasTDI/P7/correctregions.py
+++ PracticasTDI/P7/correctregions.py
@@ -13,15 +13,15 @@ def correctregions(img, markers):
     
     for i in range(0, frameH):
         for j in range(0,frameW):
-        
-            for m in range(-1,2):
-                for n in range(-1,2):
-                    if 0<i+m<frameH:
-                        if 0<j+n<frameW:
-                            if markers[i+m,j+n] != markers[i,j]:
-                        
-                                if img[i,j]==img[i+m,j+n]:
-                                    markers[i,j] = markers[i+m,j+n]
+            if markers[i,j] != 0:
+                for m in range(-1,2):
+                    for n in range(-1,2):
+                        if 0<i+m<frameH:
+                            if 0<j+n<frameW:
+                                if markers[i+m,j+n] != markers[i,j]:
+                            
+                                    if img[i,j]==img[i+m,j+n]:
+                                        markers[i+m,j+n] = markers[i,j]
                         
     return markers
 
@@ -49,24 +49,36 @@ def findborder(markers):
 
 if __name__ == "__main__":
     
-    img = cv2.imread('minimos.png',0)
+    img = cv2.imread('prueba_min.png',0)
     img = 255-img
     frameH, frameW = img.shape
     conn = np.ones((3,3))
-    binary = peak_local_max(img, footprint=conn, indices=False, exclude_border=0)
+    imgfilter =  cv2.medianBlur(img, 11)
+    binary = peak_local_max(imgfilter, footprint=conn, indices=False, exclude_border=0)
     binary = np.uint8(binary)*255
     ret, markers = cv2.connectedComponents(binary)
     
-    markers = correctregions(img, markers)
-    border = findborder(markers)
+    newmarkers = correctregions(img, markers)
+    border = findborder(newmarkers)
     new = np.zeros([frameH,frameW,3], np.uint8)
     
-    #for x in range(0, len(border)):
-     #   a=border[x][0]
-     #   b=border[x][1]
-     #   new[a,b] = [255,0,0]
+    for x in range(0, len(border)):
+        a=border[x][0]
+        b=border[x][1]
+        new[a,b] = [255,0,0]
+        
+    plt.figure('border')
+    plt.imshow(new)
     
+    plt.figure('image')
+    plt.imshow(img, cmap='gray')
+    
+    plt.figure('filter')
+    plt.imshow(imgfilter, cmap='gray')
+    
+    plt.figure('markers')
+    plt.imshow(markers, cmap=plt.cm.get_cmap('nipy_spectral'))
     plt.figure('new markers')
     plt.imshow(new)
-    plt.imshow(markers, cmap=plt.cm.get_cmap('nipy_spectral'))
+    plt.imshow(newmarkers, cmap=plt.cm.get_cmap('nipy_spectral'))
     
